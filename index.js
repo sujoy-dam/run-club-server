058c677335ff71babbe7968facc5420516b756cd
sujoy-dam <sujooy01@gmail.com>
2024-12-24 23:56:07 +0600

    update marathon data api created

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ async function run() {
     const result = await marathonsCollection.deleteOne(query)
     res.send(result);
   })
+  // update marathon data api 
+  app.put('/update-marathon/:id', async(req,res)=>{
+    const id= req.params.id;
+    const filter= {
+      _id:new ObjectId(id)
+    }
+    const options ={upsert:true}
+    const updatedMarathon = req.body;
+    const marathon={
+      $set:updatedMarathon
+    }
+    const result = await marathonsCollection.updateOne(filter, marathon,options)
+    res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
