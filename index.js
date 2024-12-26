d9f21e9105a10e5804913beb95d9b9f48fc70aa3
sujoy-dam <sujooy01@gmail.com>
2024-12-27 00:43:51 +0600

    my apply list update api created

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function run() {
 
     const marathonsCollection = client.db("marathonDB").collection('marathons')
     const appliesCollection = client.db("marathonDB").collection('applies')
-    
+
     // marathons api 
 
     // marathon post api 
@@ -41,49 +41,88 @@ async function run() {
       const cursor = marathonsCollection.find()
       const result = await cursor.toArray()
       res.send(result)
-  })
-  // get single marathon data api 
-  app.get('/marathon/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query={_id: new ObjectId(id)}
-    const result = await marathonsCollection.findOne(query)
-    res.send(result)
-  })
-  // get email based my marathons list 
-  app.get('/marathons/:email', async(req,res)=>{
-    const email=req.params.email;
-    const query = {email:email}
-    const result = await marathonsCollection.find(query).toArray()
-    res.send(result)
-  })
-  // marathons delete api 
-  app.delete('/marathon/:id', async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id:new ObjectId(id)}
-    const result = await marathonsCollection.deleteOne(query)
-    res.send(result);
-  })
-  // update marathon data api 
-  app.put('/update-marathon/:id', async(req,res)=>{
-    const id= req.params.id;
-    const filter= {
-      _id:new ObjectId(id)
-    }
-    const options ={upsert:true}
-    const updatedMarathon = req.body;
-    const marathon={
-      $set:updatedMarathon
-    }
-    const result = await marathonsCollection.updateOne(filter, marathon,options)
-    res.send(result)
-  })
+    })
+    // get single marathon data api 
+    app.get('/marathon/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await marathonsCollection.findOne(query)
+      res.send(result)
+    })
+    // get email based my marathons list 
+    app.get('/marathons/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await marathonsCollection.find(query).toArray()
+      res.send(result)
+    })
+    // marathons delete api 
+    app.delete('/marathon/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await marathonsCollection.deleteOne(query)
+      res.send(result);
+    })
+    // update marathon data api 
+    app.put('/update-marathon/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {
+        _id: new ObjectId(id)
+      }
+      const options = { upsert: true }
+      const updatedMarathon = req.body;
+      const marathon = {
+        $set: updatedMarathon
+      }
+      const result = await marathonsCollection.updateOne(filter, marathon, options)
+      res.send(result)
+    })
 
-  // my apply api
-  app.post('/apply', async(req,res)=>{
-    const newApply = req.body
-    const result = await appliesCollection.insertOne(newApply)
-    res.send(result)
-  })
+    // my apply api
+    app.post('/apply', async (req, res) => {
+      const newApply = req.body
+      const result = await appliesCollection.insertOne(newApply)
+
+      const filter = { _id: new ObjectId(newApply.marathonId) }
+      const update = {
+        $inc: { registration_count: 1 },
+      }
+      const updateRegisterCount = await marathonsCollection.updateOne(filter, update)
+      res.send(result)
+    })
+    app.get('/apply/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await appliesCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/applies/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+
+      const result = await appliesCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.delete('/apply/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await appliesCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.put('/update-apply/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {
+        _id: new ObjectId(id)
+      }
+      const options = { upsert: true }
+      const updatedData = req.body;
+      const apply = {
+        $set: updatedData,
+      }
+      const result = await appliesCollection.updateOne(filter, apply, options)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
